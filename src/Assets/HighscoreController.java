6aa233265bebf1b4a4ef318e1f99fd3db822f1c9
Christoffer Lundström <lulleh_@hotmail.com>
2019-03-17 21:05:41 +0100

    Added Test documentation together with Time log and screenshots. Unit test edited to include future function implementation.
    Signed-off-by: Christoffer Lundström <lulleh_@hotmail.com>

--- src/Assets/HighscoreController.java
+++ src/Assets/HighscoreController.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 
 public class HighscoreController {
